dad222005e716585745fa40ab1955069fac97ca8
gkaushik2809 <gkaushik2809@gmail.com>
2026-05-03 14:24:23 +0530

    Create ContainsDuplicate.py

---
+++ ContainsDuplicate.py
@@ -0,0 +1,11 @@
+'''
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+'''
+
+def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(nums)> len(set(nums))
+
