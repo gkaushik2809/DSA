c0c31168ac09f2472476618a7045afd5b035d90e
gkaushik2809 <gkaushik2809@gmail.com>
2026-05-03 14:27:09 +0530

    Update ContainsDuplicate.py

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -7,5 +7,5 @@ def containsDuplicate(self, nums):
         :type nums: List[int]
         :rtype: bool
         """
-        return len(nums)> len(set(nums))
+        return len(nums)!= len(set(nums))
 
